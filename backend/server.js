9d2a8d7422c38fe71a059a3709087af1fca3ffed
Fanis Chamalidis <fchamalidis@yahoo.com>
2023-10-26 11:11:37 +0300

    Connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
-
 const port = process.env.PORT || 5000;
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
