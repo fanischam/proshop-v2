a33ec95480f614f024bbb8341713e53374fba28a
fanischam <fchamalidis@yahoo.com>
2023-11-01 11:04:32 +0200

    created custom error messages

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -13,6 +14,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
