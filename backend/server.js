8400ffaf3fb78059d9435496cc8b945eb7d1bb4b
fanischam <fchamalidis@yahoo.com>
2023-11-01 10:42:05 +0200

    Reading products from database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
 import dotenv from 'dotenv';
 dotenv.config();
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
@@ -11,14 +12,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
