c292afcd1ed3d4362c3b2d4ed357b8a9c37098c6
fanischam <fchamalidis@yahoo.com>
2023-11-03 10:16:03 +0200

    created products slice

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,15 @@
+import { PRODUCTS_URL } from '../constants';
+import { api } from './apiSlice';
+
+export const productsApiSlice = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productsApiSlice;
