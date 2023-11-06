b3fef25656ab4533d5f139f3df6b5c87d1fc3aab
fanischam <fchamalidis@yahoo.com>
2023-11-06 11:35:18 +0200

    Created Cart Funcionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from './slices/apiSlice';
+import cartSliceReducer from './slices/cartSlice';
 
 const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
+    cart: cartSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
