fe00503bd5f71a32c8f0eed7810ad036ef07d4a3
Fanis Chamalidis <fchamalidis@yahoo.com>
2024-08-11 14:15:33 +0300

    Added login user authentication

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,21 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 dotenv.config();
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
