5c44d8797f123afa8c237da286e65eed09374ae5
Fanis Chamalidis <fchamalidis@yahoo.com>
2024-08-14 13:13:36 +0300

    Created logout functionality

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -10,7 +10,13 @@ export const usersApiSlice = api.injectEndpoints({
         body: data,
       }),
     }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: 'POST',
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
